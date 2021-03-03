5a43eaed22f1b72b70fbd59f63ef69358f158574
Vikram Singh Saini <sspl.vssaini@gmail.com>
2021-03-03 07:08:57 +0530

    Add project files.

---
+++ ServerToAD/FrmMain.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace ServerToAD
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.lblDcIpAddress = new System.Windows.Forms.Label();
+            this.txtDcIpAddress = new System.Windows.Forms.TextBox();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.AutoSize = true;
+            this.lblHeader.Location = new System.Drawing.Point(12, 16);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(325, 15);
+            this.lblHeader.TabIndex = 0;
+            this.lblHeader.Text = "Perform AD queries from server which is not part of domain.";
+            // 
+            // lblDcIpAddress
+            // 
+            this.lblDcIpAddress.AutoSize = true;
+            this.lblDcIpAddress.Location = new System.Drawing.Point(12, 54);
+            this.lblDcIpAddress.Name = "lblDcIpAddress";
+            this.lblDcIpAddress.Size = new System.Drawing.Size(84, 15);
+            this.lblDcIpAddress.TabIndex = 1;
+            this.lblDcIpAddress.Text = "DC IP Address:";
+            // 
+            // txtDcIpAddress
+            // 
+            this.txtDcIpAddress.Location = new System.Drawing.Point(102, 46);
+            this.txtDcIpAddress.Name = "txtDcIpAddress";
+            this.txtDcIpAddress.Size = new System.Drawing.Size(159, 23);
+            this.txtDcIpAddress.TabIndex = 2;
+            this.txtDcIpAddress.Text = "192.168.1.84";
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(12, 87);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(63, 15);
+            this.lblUsername.TabIndex = 3;
+            this.lblUsername.Text = "Username:";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(102, 84);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(159, 23);
+            this.txtUsername.TabIndex = 4;
+            this.txtUsername.Text = "Domain\\Administrator";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(12, 129);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(60, 15);
+            this.lblPassword.TabIndex = 5;
+            this.lblPassword.Text = "Password:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(102, 121);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(159, 23);
+            this.txtPassword.TabIndex = 6;
+            this.txtPassword.Text = "Pass99";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(262, 152);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 7;
+            this.btnConnect.Text = "&Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // FrmMain
+            // 
+            this.AcceptButton = this.btnConnect;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 182);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.txtDcIpAddress);
+            this.Controls.Add(this.lblDcIpAddress);
+            this.Controls.Add(this.lblHeader);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Server to DC or AD";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.Label lblDcIpAddress;
+        private System.Windows.Forms.TextBox txtDcIpAddress;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnConnect;
+    }
+}
+
---
+++ ServerToAD/FrmMain.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+using System.DirectoryServices;
+using System.Windows.Forms;
+
+namespace ServerToAD
+{
+    public partial class FrmMain : Form
+    {
+        private bool _isConnected;
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs args)
+        {
+            try
+            {
+                // create LDAP connection object 
+                var ldapPath = string.Format("LDAP://{0}/CN=Users,DC=domain,DC=com", txtDcIpAddress.Text);
+                var ldapConnection = new DirectoryEntry
+                {
+                    Path = ldapPath,
+                    AuthenticationType = AuthenticationTypes.Secure,
+                    Username = txtUsername.Text,
+                    Password = txtPassword.Text
+                };
+
+                // create search object which operates on LDAP connection object  
+                // and set search object to only find the user specified  
+
+                var search = new DirectorySearcher(ldapConnection) {Filter = "(cn=Administrator)"};
+
+                // create results objects from search object  
+
+                SearchResult result = search.FindOne();
+
+                if (result != null)
+                {
+                    _isConnected = true;
+                    // user exists, cycle through LDAP fields (cn, telephonenumber etc.)  
+
+                    ResultPropertyCollection fields = result.Properties;
+
+                    if (fields.PropertyNames != null)
+                        foreach (String ldapField in fields.PropertyNames)
+                        {
+                            // cycle through objects in each field e.g. group membership  
+                            // (for many fields there will only be one object such as name)  
+
+                            foreach (Object myCollection in fields[ldapField])
+                                Debug.WriteLine("{0,-20} : {1}", ldapField, myCollection);
+                        }
+                }
+
+                else
+                {
+                    // user does not exist  
+                    MessageBox.Show("User not found!");
+                }
+
+                MessageBox.Show(_isConnected ? "Got connected to AD successfully!" : "Cannot connect to AD.");
+            }
+
+            catch (Exception e)
+            {
+                MessageBox.Show("Exception caught:\n\n" + e); 
+            }  
+        }
+    }
+}
